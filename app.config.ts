ebcdda36592b0831c79aca678c62d938ed5e25d6
Daniel <dmoralesgonzalez97@gmail.com>
2025-07-10 10:44:56 +0200

    bumps expo@53

--- app.config.ts
+++ app.config.ts
@@ -1,37 +1,36 @@
-import { ConfigContext, ExpoConfig } from 'expo/config';
+import { ConfigContext, ExpoConfig } from "expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: 'auth0-sso-test',
-  slug: 'auth0-sso-test',
-  version: '1.0.0',
-  orientation: 'portrait',
-  userInterfaceStyle: 'automatic',
-  assetBundlePatterns: ['**/*'],
+  name: "auth0-sso-test",
+  slug: "auth0-sso-test",
+  version: "1.0.0",
+  orientation: "portrait",
+  userInterfaceStyle: "automatic",
+  assetBundlePatterns: ["**/*"],
   splash: {
     image: "./assets/splash.png",
     resizeMode: "contain",
-    backgroundColor: "#ffffff"
+    backgroundColor: "#ffffff",
   },
   ios: {
     supportsTablet: true,
-    bundleIdentifier: "com.joe.test"
+    bundleIdentifier: "com.joejuice.joeloyalty",
   },
   android: {
     adaptiveIcon: {
-      "foregroundImage": "./assets/adaptive-icon.png",
-      "backgroundColor": "#ffffff"
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
     },
-    package: "com.joe.test"
+    package: "com.joejuice.joeloyalty",
   },
   plugins: [
     [
-      'react-native-auth0',
+      "react-native-auth0",
       {
         clientId: process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID,
-        domain: process.env.EXPO_PUBLIC_AUTH0_DOMAIN
+        domain: process.env.EXPO_PUBLIC_AUTH0_DOMAIN,
       },
     ],
   ],
 });
-
